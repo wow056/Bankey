776cfebbf025b31c4a6ccbaf6fedbba16ee18308
diana <diana@watcha.com>
2022-07-23 14:07:23 +0900

    Add first onboarding screen

---
+++ Bankey/Onboarding/OnBoardingViewController.swift
@@ -0,0 +1,58 @@
+//
+//  OnBoardingViewController.swift
+//  Bankey
+//
+//  Created by 강민수 on 2022/07/23.
+//
+
+import Foundation
+import UIKit
+
+class OnBoardingViewController: UIViewController {
+    
+    let stackView = UIStackView()
+    let imageView = UIImageView()
+    let label = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+
+extension OnBoardingViewController {
+    func style() {
+        view.backgroundColor = .systemBackground // ! dont forget
+    
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "delorean")
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Welcome"
+        label.textAlignment = .center
+        label.font = .preferredFont(forTextStyle: .title3)
+        label.adjustsFontForContentSizeCategory = true
+        label.numberOfLines = 0
+        label.text = "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like you are back in 1989."
+    }
+    
+    func layout() {
+        stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(label)
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1)
+        ])
+    }
+}
